96a768a14c3e4331319a6e88e2c65705b44cecbd
Turan Heydarli <info@yennimusic.com>
2022-08-16 00:00:02 +0400

    Add repository pattern

---
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext:DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if (!optionsBuilder.IsConfigured)
+            //    base.OnConfiguring(
+            //        optionsBuilder.UseSqlServer(Configuration.GetConnectionString("SomeConnectionString")));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Brand>(b =>
+            {
+                b.ToTable("Brands");
+                b.Property(p => p.Id).HasColumnName("Id");
+                b.Property(p => p.Name).HasColumnName("Name");
+                
+            });
+
+            Brand[] brandEntitySeeds = { new Brand { Id = 1, Name= "BMW"}, new Brand { Id = 2, Name = "Mec" } };
+
+            modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
+        }
+    }
+}
