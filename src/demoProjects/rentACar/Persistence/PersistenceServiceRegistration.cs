4d6365d0a11a2459bd226ad0f31573c30f1632b4
Turan Heydarli <info@yennimusic.com>
2022-08-22 23:52:12 +0400

    Create brand feature

--- src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
+++ src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
@@ -4,11 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Contexts;
 using Persistence.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Persistence
 {
